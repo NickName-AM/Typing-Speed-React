c70bb7411b2bf8f4f7931958646b20f16b37d7f3
NickName-AM <maharjanebik061@gmail.com>
2025-02-06 13:53:46 +0545

    golang + react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HomePage from './pages/Homepage'
+import Typer from './Typer'
+
+function App() {
+
+  return (
+    <Typer />
+    // <HomePage/>
+  )
+}
+
+export default App
